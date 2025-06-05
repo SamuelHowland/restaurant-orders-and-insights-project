8f2df0d9c1c168525c27b9d8241a21845cedde0c
SamuelHowland <samhowlandwork@gmail.com>
2025-06-05 11:04:46 +0100

    Delete 01_clean_restaurants.sql

--- 01_clean_restaurants.sql
+++
@@ -1,9 +0,0 @@
--- Cleaning restaurant_name and cuisine fields
-CREATE TABLE restaurants AS 
-SELECT 
-	restaurant_id,
-	TRIM(restaurant_name) AS restaurant_name,
-	INITCAP(cuisine) AS cuisine,
-	zone,
-	category
-FROM dirty_restaurants dr
